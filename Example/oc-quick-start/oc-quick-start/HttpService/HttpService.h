6145ce9ec65f00908aa7cb1484bbba7940c1e17d
coooliang <wot_chenliang_xdl@cibdev.com>
2023-09-27 15:48:48 +0800

    add project

---
+++ Example/oc-quick-start/oc-quick-start/HttpService/HttpService.h
@@ -0,0 +1,15 @@
+//// 
+//  HttpService.h
+//  oc-quick-start
+//  Created by ___ORGANIZATIONNAME___ on 2023/9/27
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HttpService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/oc-quick-start/oc-quick-start/HttpService/HttpService.m
@@ -0,0 +1,16 @@
+//// 
+//  HttpService.m
+//  oc-quick-start
+//  Created by ___ORGANIZATIONNAME___ on 2023/9/27
+//
+
+#import "HttpService.h"
+#import <AFNetworking.h>
+
+@implementation HttpService
+
+- (void)post:(NSString *)url parameters:(NSDictionary *)parameters {
+    afnetworkingman
+}
+
+@end
