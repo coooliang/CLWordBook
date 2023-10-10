5e8bc5f5c2fb7e9bd92588c741ea12e688d5b58b
coooliang <wot_chenliang_xdl@cibdev.com>
2023-10-10 15:15:20 +0800

    update to csv

--- Example/oc-quick-start/oc-quick-start/VCS/MyViewController.m
+++ Example/oc-quick-start/oc-quick-start/VCS/MyViewController.m
@@ -18,13 +18,27 @@
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.blueColor;
     
-    NSString *path = [[NSBundle mainBundle]pathForResource:@"GRE_8000_Words" ofType:@"csv"];
-    NSError *error = nil;
-    NSString *fileContents = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
     
-    NSArray *arr = [fileContents componentsSeparatedByString:@"\r\n"];
-    NSLog(@"arr = %@",arr);
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"Middle_School" ofType:@"csv"];
+    NSError *error = nil;
+    NSString *fileContents = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+    NSArray *arr = [fileContents componentsSeparatedByString:@"\n"];
+    if(arr && [arr isKindOfClass:NSArray.class] && arr.count > 1){
+        for (NSString *line in arr) {
+            if(line && line.length > 1){
+                NSArray *ws = [line componentsSeparatedByString:@","];
+                if(ws.count == 3){
+                    
+                }else{
+                    NSLog(@"error = %@",ws);
+                }
+            }
+        }
+    }
+}
 
 @end
